e2e4e7cf09aebace2704a36c78a86faaa0b3ff89
yemelyn <pommy.shortcake@gmail.com>
2016-07-19 02:19:54 +0000

    Added reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :reviews
 
   validates :title, presence: true
   validates :director, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
